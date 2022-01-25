0502392fe9c8138afc3f26518a73adfad560faf2
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-25 11:57:43 -0600

    Completed simple project

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+//Welcome user to application
+Console.WriteLine("Welcome to the Greeting Application");
+Console.WriteLine("This application was built by Anthony Padgett");
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine();
+
+//Ask them their first name
+Console.Write("What is your first name: ");
+string firstName;
+firstName = Console.ReadLine();
+
+
+//Greet them by name
+Console.WriteLine();
+Console.WriteLine($"Hello {firstName}");
+
+Console.WriteLine("Thank you for using my application.");
+Console.ReadLine();
