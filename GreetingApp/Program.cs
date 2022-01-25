6131fbd256f9e9edac3bb3b05ece3d917894f5bd
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-25 11:59:05 -0600

    simple Project done

--- GreetingApp/Program.cs
+++ GreetingApp/Program.cs
@@ -3,7 +3,7 @@
 //Welcome user to application
 Console.WriteLine("Welcome to the Greeting Application");
 Console.WriteLine("This application was built by Anthony Padgett");
-Console.WriteLine("---------------------------------------------");
+Console.WriteLine("--------------------------------------------_-");
 Console.WriteLine();
 
 //Ask them their first name
